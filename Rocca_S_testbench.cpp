893d653bcd1773f6413ff2c1e2feeee13dc8f3c8
Rohan Basugade <rohanbasugade@gmail.com>
2024-09-07 15:33:44 +0530

    LE128 function added

--- Rocca_S_testbench.cpp
+++ Rocca_S_testbench.cpp
@@ -5,10 +5,19 @@
 using namespace std;
 
 __uint128_t S[7]; // state array
-__uint128_t N;
+__uint128_t N; // 12 octets to 16 octets(since its always padded to 128 bits before use stored it as 128bit value)
 __uint128_t Z0 = 0x428a2f98d728ae227137449123ef65cd;
 __uint128_t Z1 = 0xb5c0fbcfec4d3b2fe9b5dba58189dbbc;
 __uint128_t K[2];
+
+__uint128_t AD[64]; //max length = 2^62 octets = 2^61 * 8 bits
+__uint128_t Size_AD;
+__uint128_t M[256]; //max length = 2^125 octets = 2^125 * 8 bits
+__uint128_t Size_M;
+__uint128_t C[256]; 
+__uint128_t T[2];
+
+ 
 
 // to pad a hex string to size mod(256)=0
 int PAD(const char *inp, uint8_t *byteArray)
@@ -95,8 +104,56 @@ void initialize()
     S[6] = S[6] ^ K[1];
 }
 
+void proccessAD(__uint128_t *AD)
+{
+    for (int i = 0; i < sizeof(AD); i = i + 2)
+    {
+        roundFunction(AD[i], AD[i + 1]);
+    }
+}
+
+void Rocca_S_encrypt(__uint128_t *M)
+{
+    for (int i = 0; i < sizeof(M); i=i+2)
+    {
+        C[i] = AES(S[3] ^ S[5], S[0]) ^ M[i];
+        C[i] = AES(S[4] ^ S[6], S[2]) ^ M[i+1];
+        roundFunction(M[i], M[i + 1]);
+    }
+}
+
+__uint128_t LE128(__uint128_t num){
+    __uint128_t out = 0;
+    for (int i = 0; i < 16; i++){
+        out = out << 8;
+        out = out | (num & 0xFF);
+        num = num >> 8;
+    }
+    return out;
+}
+
+void finalize(){
+    for (int i = 0; i < 16; i++){
+        roundFunction(LE128(Size_AD), LE128(Size_M));
+    }
+
+    T[0] = (S[0] ^ S[1] ^ S[2] ^ S[3]);
+    T[1] = (S[4] ^ S[5] ^ S[6]);
+}
+
+
 int main()
 {
     initialize();
+
     printf("%llx\n", (unsigned long long)S[6]);
+
+    PAD(AD); // need to implement
+    proccessAD(AD);
+
+    PAD(M);
+    Rocca_S_encrypt(M);
+
+    finalize();
+
 }
